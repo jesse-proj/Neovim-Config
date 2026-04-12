74552a1413a84e128b9c9eb971a643d0076acd2a
jesse-proj <jian.santiago39@gmail.com>
2026-04-12 13:57:49 +0800

    Added more mini plugins.

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,54 +1,6 @@
-return { -- Collection of various small independent plugins/modules
-    'nvim-mini/mini.nvim',
-    config = function()
-      -- Better Around/Inside textobjects
-      --
-      -- Examples:
-      --  - va)  - [V]isually select [A]round [)]paren
-      --  - yinq - [Y]ank [I]nside [N]ext [Q]uote
-      --  - ci'  - [C]hange [I]nside [']quote
-      require('mini.ai').setup { n_lines = 500 }
-
-      -- Add/delete/replace surroundings (brackets, quotes, etc.)
-      --
-      -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
-      -- - sd'   - [S]urround [D]elete [']quotes
-      -- - sr)'  - [S]urround [R]eplace [)] [']
-      require('mini.surround').setup()
-      require('mini.comment').setup()
-      require('mini.pairs').setup({
-        ['('] = { action = 'open', pair = '()', neigh_pattern = '^[^\\]' },
-        ['['] = { action = 'open', pair = '[]', neigh_pattern = '^[^\\]' },
-        ['{'] = { action = 'open', pair = '{}', neigh_pattern = '^[^\\]' },
-
-        [')'] = { action = 'close', pair = '()', neigh_pattern = '^[^\\]' },
-        [']'] = { action = 'close', pair = '[]', neigh_pattern = '^[^\\]' },
-        ['}'] = { action = 'close', pair = '{}', neigh_pattern = '^[^\\]' },
-
-        ['"'] = { action = 'closeopen', pair = '""', neigh_pattern = '^[^\\]',   register = { cr = false } },
-        ["'"] = { action = 'closeopen', pair = "''", neigh_pattern = '^[^%a\\]', register = { cr = false } },
-        ['`'] = { action = 'closeopen', pair = '``', neigh_pattern = '^[^\\]',   register = { cr = false } },
-      })
-      require('mini.sessions').setup()
-      require('mini.indentscope').setup({
-        draw = {
-          delay = 100, 
-
-          animation = require('mini.indentscope').gen_animation.linear({ 
-            duration = 5, 
-            unit = 'step' 
-          }),
-        },
-
-        symbol = '│', -- Symbol character
-        options = { try_as_border = true },
-      })
-
-      local starter = require('mini.starter')
-
-      -- PenVim ASCII Art (Standard Style)
-      local my_header = [[
-       ____         __     ___          
+local function setup_starter(starter)
+      local header_text = [[
+       ____         __     ___         
       |  _ \ ___ _ _\ \   / (_)_ __ ___ 
       | |_) / _ \ '_ \ \ / /| | '_ ` _ \
       |  __/  __/ | | \ V / | | | | | | |
@@ -59,15 +11,13 @@ return { -- Collection of various small independent plugins/modules
       vim.api.nvim_create_autocmd('User', {
         pattern = 'MiniStarterOpened',
         callback = function()
-          -- Set fillchars to hide eob only in the current starter window
           vim.wo.fillchars = 'eob: '
         end,
       })
 
       starter.setup({
-        header = my_header,
+        header = header_text,
 
-        -- This changes the instruction text at the very bottom
         footer = 'Type to filter  |  <CR> to select',
 
         sections = {
@@ -95,8 +45,9 @@ return { -- Collection of various small independent plugins/modules
           end
         end,
       })
+end
 
-
+local function setup_sessions() 
       vim.keymap.set('n', '<leader>wsl', function()
         MiniSessions.write('Session.vim')
       end, { desc = '[W]orkspace [S]ave [L]ocal'}) 
@@ -105,30 +56,64 @@ return { -- Collection of various small independent plugins/modules
         MiniSessions.write('LastSession.vim')
       end, { desc = '[W]orkspace [S]ave [G]lobal'}) 
 
-      -- Option A: Link to an existing Oxocarbon highlight group
-      vim.api.nvim_set_hl(0, 'MiniIndentscopeSymbol', { link = 'Comment' })
-
       vim.keymap.set('n', '<leader>wo', MiniSessions.select, { desc = '[W]orkspace [O]pen'})
     
       vim.keymap.set('n', '<leader>wk', function()
         local name = vim.fn.input('Delete session: ')
         if name ~= '' then MiniSessions.delete(name) end
       end, { desc = '[W]orkspace [K]ill' })
+end
 
-      -- Simple and easy statusline.
-      --  You could remove this setup call if you don't like it,
-      --  and try some other statusline plugin
-      local statusline = require 'mini.statusline'
-      -- set use_icons to true if you have a Nerd Font
+local function setup_indentscope(indentscope)
+      indentscope.setup({
+        draw = {
+          delay = 100, 
+
+          animation = require('mini.indentscope').gen_animation.linear({ 
+            duration = 5, 
+            unit = 'step' 
+          }),
+        },
+
+        symbol = '│', -- Symbol character
+        options = { try_as_border = true },
+      })
+
+      vim.api.nvim_set_hl(0, 'MiniIndentscopeSymbol', { link = 'Comment' })
+end
+
+local function setup_statusline(statusline) 
       statusline.setup { use_icons = vim.g.have_nerd_font }
 
-      -- You can configure sections in the statusline by overriding their
-      -- default behavior. For example, here we set the section for
-      -- cursor location to LINE:COLUMN
-      ---@diagnostic disable-next-line: duplicate-set-field
       statusline.section_location = function() return '%2l:%-2v' end
+end
 
-      -- ... and there is more!
-      --  Check out: https://github.com/nvim-mini/mini.nvim
+return { 
+      'nvim-mini/mini.nvim',
+      config = function()
+
+      -- Add/delete/replace surroundings (brackets, quotes, etc.)
+      --
+      -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
+      -- - sd'   - [S]urround [D]elete [']quotes
+      -- - sr)'  - [S]urround [R]eplace [)] [']
+    
+      require('mini.ai').setup { n_lines = 500 }
+      require('mini.surround').setup()
+      require('mini.comment').setup()
+      require('mini.splitjoin').setup() -- gS to toggle!
+      require('mini.move').setup() 
+      require('mini.pairs').setup()
+      require('mini.sessions').setup()
+
+      local starter           = require('mini.starter')
+      local indentscope       = require('mini.indentscope')
+      local statusline        = require('mini.statusline')
+
+      setup_sessions()
+      setup_starter(starter)
+      setup_indentscope(indentscope)
+      setup_statusline(statusline)
+
     end,
 }
